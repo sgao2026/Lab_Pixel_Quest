ea5d46ffc8ef44c139fb532ab22ff2e0ade68c15
Sandra Gao <sgao60@stuy.edu>
2025-02-15 11:10:55 -0500

    step variable to make changing scale easier

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 
 public class GeoController : MonoBehaviour
 {
     string greet0 = "Hello";
     // int var = 3;
-    private Vector3 up = new Vector3(0, 1, 0);
-    private Vector3 down = new Vector3(0, -1, 0);
-    private Vector3 right = new Vector3(1, 0, 0);
-    private Vector3 left = new Vector3(-1, 0, 0);
+    private static float step = 0.005f;
+    private Vector3 up = new Vector3(0, step, 0);
+    private Vector3 down = new Vector3(0, -1 * step, 0);
+    private Vector3 right = new Vector3(step, 0, 0);
+    private Vector3 left = new Vector3(-1 * step, 0, 0);
     
     // Start is called before the first frame update
     void Start()
