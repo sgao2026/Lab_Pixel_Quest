1911e093383efdb13f6bf2d038f3352a7c1f26fb
Sandra Gao <sgao60@stuy.edu>
2025-02-15 10:38:04 -0500

    testing vars

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class GeoController : MonoBehaviour
 {
     string greet0 = "Hello";
+    int var = 3;
     
     // Start is called before the first frame update
     void Start()
     {
         string greet1 = "World";
-        Debug.Log(greet0 + greet1);
+        Debug.Log(greet0 + " " + greet1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(var);
+        Debug.Log(transform.position);
+        var++;
     }
 }
