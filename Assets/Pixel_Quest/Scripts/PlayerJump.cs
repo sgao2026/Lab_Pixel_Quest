4a5a3d94a6c3610977423f0011470c99a49ca99e
Sandra Gao <sgao60@stuy.edu>
2025-03-22 13:56:23 -0400

    start jump

--- Assets/Pixel_Quest/Scripts/PlayerJump.cs
+++ Assets/Pixel_Quest/Scripts/PlayerJump.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
+    private Rigidbody2D _rb;
+    public float JumpForce = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, JumpForce);
+        }
     }
 }
