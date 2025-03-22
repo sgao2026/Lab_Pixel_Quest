7c4489b4218efbf04d6aa76046bc07f402b9436e
Sandra Gao <sgao60@stuy.edu>
2025-03-22 12:45:13 -0400

    Update PlayerMovement.cs

--- Assets/Pixel_Quest/Scripts/PlayerMovement.cs
+++ Assets/Pixel_Quest/Scripts/PlayerMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _rb;
+    private static float _step = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xInput = Input.GetAxis("Horizontal"); // horizontal movement
+        _rb.velocity = new Vector2(xInput * _step, _rb.velocity.y); // using rigidbody calculated y movement
     }
 }
