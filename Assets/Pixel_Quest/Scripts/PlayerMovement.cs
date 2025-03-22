951f2b8a1e539441f136bf671ed11188126acebe
Sandra Gao <sgao60@stuy.edu>
2025-03-22 13:25:51 -0400

    player turning

--- Assets/Pixel_Quest/Scripts/PlayerMovement.cs
+++ Assets/Pixel_Quest/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,13 +13,21 @@ public class PlayerMovement : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody2D>();
         _sr = transform.Find("Sprite").GetComponent<SpriteRenderer>();
-        if (_sr != null) Debug.Log("connection successful");
     }
 
     // Update is called once per frame
     void Update()
     {
         float xInput = Input.GetAxis("Horizontal"); // horizontal movement
+        if (xInput > 0)
+        {
+            _sr.flipX = true; // right
+        }
+        else if (xInput < 0)
+        {
+            _sr.flipX = false; // left
+        }
+
         _rb.velocity = new Vector2(xInput * _step, _rb.velocity.y); // using rigidbody calculated y movement
     }
 }
