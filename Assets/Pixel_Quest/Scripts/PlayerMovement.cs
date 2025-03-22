0a47d269a150682cae0a95cf0444641cc36275d7
Sandra Gao <sgao60@stuy.edu>
2025-03-22 13:13:01 -0400

    connected sprite renderer to player

--- Assets/Pixel_Quest/Scripts/PlayerMovement.cs
+++ Assets/Pixel_Quest/Scripts/PlayerMovement.cs
@@ -6,10 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rb;
     private static float _step = 5f;
+    private SpriteRenderer _sr;
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _sr = transform.Find("Sprite").GetComponent<SpriteRenderer>();
+        if (_sr != null) Debug.Log("connection successful");
     }
 
     // Update is called once per frame
