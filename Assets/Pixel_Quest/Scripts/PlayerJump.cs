9eb26611a2a78e2b2d5e1f451970daaa0107b0ce
Sandra Gao <sgao60@stuy.edu>
2025-03-29 13:22:58 -0400

    implementing solutions for infinite jump, jumping in water

--- Assets/Pixel_Quest/Scripts/PlayerJump.cs
+++ Assets/Pixel_Quest/Scripts/PlayerJump.cs
@@ -16,9 +16,18 @@ public class PlayerJump : MonoBehaviour
     public LayerMask groundMask;
     private bool _groundCheck;
 
+    // forces
+    public float jumpForce = 10;
+    public float fallForce = 2;
+    private Vector2 _gravityVector;
+
+    // water
+    private bool _waterCheck;
+
     // Start is called before the first frame update
     void Start()
     {
+        _gravityVector = new Vector2(0, Physics2D.gravity.y);
         _rb = GetComponent<Rigidbody2D>();
     }
 
@@ -26,9 +35,24 @@ public class PlayerJump : MonoBehaviour
     void Update()
     {
         _groundCheck = Physics2D.OverlapCapsule(feetCollider.position, new Vector2(CapsuleHeight, CapsuleRadius), CapsuleDirection2D.Vertical, 0, groundMask);
-        if (Input.GetKeyDown(KeyCode.Space) && _groundCheck)
+        if (Input.GetKeyDown(KeyCode.Space) && (_groundCheck || _waterCheck))
         {
             _rb.velocity = new Vector2(_rb.velocity.x, JumpForce);
         }
+
+        if (_rb.velocity.y < 0 && !_waterCheck)
+        {
+            _rb.velocity += _gravityVector * (fallForce * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Water")) { _waterCheck = true; }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Water")) { _waterCheck = false; }
     }
 }
