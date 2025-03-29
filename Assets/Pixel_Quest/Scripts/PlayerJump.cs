37e85dacf182e099f00856e847f01a91bd40390a
Sandra Gao <sgao60@stuy.edu>
2025-03-29 13:10:57 -0400

    spikes order fix + bouyancy working

--- Assets/Pixel_Quest/Scripts/PlayerJump.cs
+++ Assets/Pixel_Quest/Scripts/PlayerJump.cs
@@ -6,6 +6,16 @@ public class PlayerJump : MonoBehaviour
 {
     private Rigidbody2D _rb;
     public float JumpForce = 5f;
+
+    // Capsule
+    public float CapsuleHeight = 0.25f;
+    public float CapsuleRadius = 0.08f;
+
+    // Ground Check
+    public Transform feetCollider;
+    public LayerMask groundMask;
+    private bool _groundCheck;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +25,8 @@ public class PlayerJump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        _groundCheck = Physics2D.OverlapCapsule(feetCollider.position, new Vector2(CapsuleHeight, CapsuleRadius), CapsuleDirection2D.Vertical, 0, groundMask);
+        if (Input.GetKeyDown(KeyCode.Space) && _groundCheck)
         {
             _rb.velocity = new Vector2(_rb.velocity.x, JumpForce);
         }
